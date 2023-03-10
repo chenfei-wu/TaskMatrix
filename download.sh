70fa0328e37cf5a8bfc6ee9f005e995607bcc51f
yunfeng <fdoctor00@gmail.com>
2023-03-10 16:42:19 +0800

    Update download.sh
    Make it easier to modify and maintain download.sh.

--- download.sh
+++ download.sh
@@ -3,12 +3,21 @@ ln -s ControlNet/ldm ./ldm
 ln -s ControlNet/cldm ./cldm
 ln -s ControlNet/annotator ./annotator
 cd ControlNet/models
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_depth.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_hed.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_mlsd.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_normal.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_openpose.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_scribble.pth
-wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_seg.pth
+
+function download_model() {
+    model=$1
+    path="ControlNet/models/$model"
+    url="https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/$model"
+    wget $url
+}
+
+download_model control_sd15_canny.pth
+download_model control_sd15_depth.pth
+download_model control_sd15_hed.pth
+download_model control_sd15_mlsd.pth
+download_model control_sd15_normal.pth
+download_model control_sd15_openpose.pth
+download_model control_sd15_scribble.pth
+download_model control_sd15_seg.pth
+
 cd ../../
