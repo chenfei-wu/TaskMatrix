9d8a645b1b2bfd09fd76496ca74432465c6380cc
DESKTOP-414ARHV\caomaosong <caoms@shanghaitech.edu.cn>
2023-03-23 00:03:13 +0800

    add a template of infinity_outpainting

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -1045,7 +1045,6 @@ class ConversationBot:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--load', type=str, default="ImageCaptioning_cuda:0,Text2Image_cuda:0")
-    parser.add_argument('--load', type=str, default="ImageCaptioning_cuda:1,ImageEditing_cuda:4,VisualQuestionAnswering_cuda:5")
     args = parser.parse_args()
     load_dict = {e.split('_')[0].strip(): e.split('_')[1].strip() for e in args.load.split(',')}
     bot = ConversationBot(load_dict=load_dict)
